80c0688ec5758742ee268b2560a37860c9c602db
Jamshid Qayimov <jamshidqayimov0399@gmail.com>
2025-01-08 17:19:10 +0500

    hal

--- components/hero/HeaderSwiper.jsx
+++ components/hero/HeaderSwiper.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const HeaderSwiper = () => {
   const params = useParams();
