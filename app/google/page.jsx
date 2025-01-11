a8950671363dbd494a02a1796bf23f061e269624
Jamshid Qayimov <jamshidqayimov0399@gmail.com>
2025-01-11 16:46:13 +0500

    push

--- app/google/page.jsx
+++ app/google/page.jsx
@@ -18,6 +18,7 @@ export default function Google() {
           {
             params: {
               code,
+              redirect_url: "https://wepapp.uzpin.games/google",
             },
           }
         );
