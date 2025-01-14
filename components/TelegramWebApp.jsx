4105f6c6d57eed764f99b8ffe75d4a7f9a3e32e4
Jamshid Qayimov <jamshidqayimov0399@gmail.com>
2025-01-14 14:33:21 +0500

    push

--- components/TelegramWebApp.jsx
+++ components/TelegramWebApp.jsx
@@ -1,32 +1,18 @@
-import Script from "next/script";
 import { useEffect } from "react";
 
 const TelegramWebApp = () => {
   useEffect(() => {
-    // Script yuklangandan so'ng Telegram SDK bilan ishlash
-    if (typeof window !== "undefined" && window.Telegram) {
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-web-app.js";
+    script.onload = () => {
       const tg = window.Telegram.WebApp;
-
-      // Telegramni ishga tushirish
       tg.ready();
-
-      // Foydalanuvchi ma'lumotlarini konsolga chiqarish
-      console.log("Telegram WebApp user:", tg.initDataUnsafe?.user);
-
-      // Telegram interfeys ranglarini o'zgartirish (ixtiyoriy)
-      tg.themeParams && console.log("Theme params:", tg.themeParams);
-    }
+      console.log("Telegram WebApp foydalanuvchi:", tg.initDataUnsafe?.user);
+    };
+    document.head.appendChild(script);
   }, []);
 
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <Script
-        src="https://telegram.org/js/telegram-web-app.js?56"
-        strategy="afterInteractive"
-        onLoad={() => console.log("Telegram WebApp SDK yuklandi.")}
-      />
-    </div>
-  );
+  return <div style={{ textAlign: "center", marginTop: "50px" }}>aaa</div>;
 };
 
 export default TelegramWebApp;
