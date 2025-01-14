e1dc97b6400ac8d6cef98f854f00a206eedc70b0
Jamshid Qayimov <jamshidqayimov0399@gmail.com>
2025-01-14 12:35:31 +0500

    test

--- components/ClientProvider.jsx
+++ components/ClientProvider.jsx
@@ -9,11 +9,9 @@ const ClientProvider = ({ children }) => {
     const storedLanguage = localStorage.getItem("language") || "uz";
     i18n.changeLanguage(storedLanguage);
 
-    if (window.Telegram?.WebApp) {
-      const telegram = window.Telegram.WebApp;
-      console.log(telegram.initDataUnsafe, "test telegram");
-      console.log(telegram.initData, "test telegram2");
-    }
+    const telegram = window.Telegram.WebApp;
+    console.log(telegram.initDataUnsafe, "test telegram");
+    console.log(telegram.initData, "test telegram2");
   }, []);
   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 };
