1e78efa8323a243a72365dfc9702df972ea9bd93
Jamshid Qayimov <jamshidqayimov0399@gmail.com>
2025-01-14 14:23:03 +0500

    next

--- components/ClientProvider.jsx
+++ components/ClientProvider.jsx
@@ -9,14 +9,13 @@ const ClientProvider = ({ children }) => {
     const storedLanguage = localStorage.getItem("language") || "uz";
     i18n.changeLanguage(storedLanguage);
 
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-web-app.js?56";
-    script.async = true;
-    document.head.appendChild(script);
+    const tg = window.Telegram.WebApp;
 
-    const telegram = window.Telegram.WebApp;
-    alert(telegram.initDataUnsafe, "test telegram");
-    alert(telegram.initData, "test telegram2");
+    // Telegramni ishga tushirish
+    tg.ready();
+    console.log(tg.initDataUnsafe?.user);
+    // alert(telegram.initDataUnsafe, "test telegram");
+    // alert(telegram.initData, "test telegram2");
   }, []);
   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 };
---
+++ next/script.jsx
@@ -0,0 +1,11 @@
+import Script from "next/script";
+
+const TelegramWebApp = () => {
+  return (
+    <div>
+      <Script src="https://telegram.org/js/telegram-web-app.js" />
+    </div>
+  );
+};
+
+export default TelegramWebApp;
