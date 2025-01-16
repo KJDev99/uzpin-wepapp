ce4fbb8389ee2cdd2a9429422221e774f4306cb2
Jamshid Qayimov <jamshidqayimov0399@gmail.com>
2025-01-16 14:04:49 +0500

    test

--- components/BottomNavbar.jsx
+++ components/BottomNavbar.jsx
@@ -49,11 +49,13 @@ export default function BottomNavbar() {
 
   useEffect(() => {
     const PostTest = async () => {
+      const formattedData = {};
       try {
         await axios.post(
           `https://api.uzpin.games/api/v1/client/webapp/botuser/${sessionStorage.getItem(
             "bot"
           )}/${sessionStorage.getItem("userId")}`,
+          formattedData,
           {
             headers: {
               Authorization: `Bearer ${token}`,
