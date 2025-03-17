390fab5fb761effe4fa6a45696dbb8e6b10bfd61
Omonov <algoritm1202@gmail.com>
2025-03-17 16:02:34 +0500

    push

--- components/login/Login.jsx
+++ components/login/Login.jsx
@@ -1,6 +1,6 @@
 import { X } from "lucide-react";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { PiEyeClosedBold } from "react-icons/pi";
 import { AiOutlineEye, AiOutlineLoading3Quarters } from "react-icons/ai";
 import { RiTelegram2Fill } from "react-icons/ri";
@@ -25,17 +25,17 @@ export default function Login({ setLogin, loginCount }) {
   });
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [referral, setReferral] = useState(null);
+  /* const [referral, setReferral] = useState(null);
   const searchParams = useSearchParams();
   useEffect(() => {
     setReferral(searchParams.get("referral"));
-  }, [searchParams]);
+  }, [searchParams]); */
   const [data, setData] = useState();
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
-  const rounter = useRouter();
+  const router = useRouter();
 
   useEffect(() => {
     const fetchBanner = async () => {
@@ -81,7 +81,7 @@ export default function Login({ setLogin, loginCount }) {
           password: password,
         });
         localStorage.setItem("profileData", JSON.stringify(response.data));
-        rounter.push("/");
+        router.push("/");
         setTimeout(() => {
           location.reload();
         }, 300);
@@ -115,8 +115,8 @@ export default function Login({ setLogin, loginCount }) {
     await signIn("apple", { callbackUrl: "/" });
   };
   const HandleTg = () => {
-    localStorage.setItem("referral", referral);
-    console.log(referral);
+    // localStorage.setItem("referral", referral);
+    // console.log(referral);
     window.location.href = `https://uzpin.games/telegram-login.html`;
   };
 
@@ -126,119 +126,123 @@ export default function Login({ setLogin, loginCount }) {
 
   return (
     <>
-      <div className="flex justify-center items-center">
-        {error && (
-          <Toast status="false" text="Kirish Jarayonida nimadir xato bo'ldi" />
-        )}
-        <div className="bg-white  rounded-lg p-8 w-full max-w-md max-sm:p-4">
-          <div className="flex relative flex-col items-center gap-4 mb-10">
-            <Link href="/">
-              <Image
-                src={data?.logo ? data.logo : "/logo.svg"}
-                className="sm:hidden"
-                width={162}
-                height={31}
-                alt="logo"
-              />
-            </Link>
-          </div>
-          <div className="flex gap-4">
-            <button
-              className={`w-[190px] h-[50px] border-none outline-none text-lg rounded-[5px] max-sm:w-[164px] ${
-                loginCount == 1
-                  ? "bg-[#313131] text-[#F9F9F9]"
-                  : "bg-[#F4F4F4] text-[#828282]"
-              }`}
-              onClick={() => setLogin(1)}
-            >
-              {t("login")}
-            </button>
-            <button
-              className={`w-[190px] h-[50px] border-none outline-none text-lg rounded-[5px] max-sm:w-[164px] ${
-                loginCount == 2
-                  ? "bg-[#313131] text-[#F9F9F9]"
-                  : "bg-[#F4F4F4] text-[#828282]"
-              }`}
-              onClick={() => setLogin(2)}
-            >
-              {t("login-text1")}
-            </button>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4 mt-5">
-              <label
-                className="block text-[#828282] text-sm px-5 pb-2"
-                htmlFor="email"
-              >
-                {t("login-text2")}
-              </label>
-              <input
-                type="text"
-                id="email"
-                placeholder="example@mail.ru"
-                value={emailOrPhone}
-                onChange={(e) => setEmailOrPhone(e.target.value)}
-                className={`w-full px-4 py-2 border rounded-lg outline-none text-[#000000] ${
-                  errors.emailOrPhone
-                    ? "border-b-2 border-[red]"
-                    : "border-[#ACACAC]"
-                }`}
-              />
-              {errors.emailOrPhone && (
-                <p className="text-red-500 text-sm mt-1 px-1">
-                  {t("login-text3")}
-                </p>
-              )}
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="flex justify-center items-center">
+          {error && (
+            <Toast
+              status="false"
+              text="Kirish Jarayonida nimadir xato bo'ldi"
+            />
+          )}
+          <div className="bg-white  rounded-lg p-8 w-full max-w-md max-sm:p-4">
+            <div className="flex relative flex-col items-center gap-4 mb-10">
+              <Link href="/">
+                <Image
+                  src={data?.logo ? data.logo : "/logo.svg"}
+                  className="sm:hidden"
+                  width={162}
+                  height={31}
+                  alt="logo"
+                />
+              </Link>
             </div>
-
-            <div className="mb-4 relative">
-              <label
-                className="block text-[#828282] text-sm px-5 pb-2"
-                htmlFor="password"
-              >
-                {t("login-text4")}
-              </label>
-              <input
-                type={passwordVisible ? "text" : "password"}
-                id="password"
-                placeholder={t("login-text4")}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className={`w-full px-4 py-2 border rounded-lg outline-none text-[#000000] ${
-                  errors.password
-                    ? "border-b-2 border-[red]"
-                    : "border-[#ACACAC]"
-                }`}
-              />
+            <div className="flex gap-4">
               <button
-                type="button"
-                onClick={togglePasswordVisibility}
-                className="absolute right-3 top-10 text-gray-500"
+                className={`w-[190px] h-[50px] border-none outline-none text-lg rounded-[5px] max-sm:w-[164px] ${
+                  loginCount == 1
+                    ? "bg-[#313131] text-[#F9F9F9]"
+                    : "bg-[#F4F4F4] text-[#828282]"
+                }`}
+                onClick={() => setLogin(1)}
               >
-                {passwordVisible ? <AiOutlineEye /> : <PiEyeClosedBold />}
+                {t("login")}
               </button>
-              {errors.password && (
-                <p className="text-red-500 text-sm mt-1 px-1">
-                  {t("login-text3")}
-                </p>
-              )}
+              <button
+                className={`w-[190px] h-[50px] border-none outline-none text-lg rounded-[5px] max-sm:w-[164px] ${
+                  loginCount == 2
+                    ? "bg-[#313131] text-[#F9F9F9]"
+                    : "bg-[#F4F4F4] text-[#828282]"
+                }`}
+                onClick={() => setLogin(2)}
+              >
+                {t("login-text1")}
+              </button>
             </div>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4 mt-5">
+                <label
+                  className="block text-[#828282] text-sm px-5 pb-2"
+                  htmlFor="email"
+                >
+                  {t("login-text2")}
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  placeholder="example@mail.ru"
+                  value={emailOrPhone}
+                  onChange={(e) => setEmailOrPhone(e.target.value)}
+                  className={`w-full px-4 py-2 border rounded-lg outline-none text-[#000000] ${
+                    errors.emailOrPhone
+                      ? "border-b-2 border-[red]"
+                      : "border-[#ACACAC]"
+                  }`}
+                />
+                {errors.emailOrPhone && (
+                  <p className="text-red-500 text-sm mt-1 px-1">
+                    {t("login-text3")}
+                  </p>
+                )}
+              </div>
 
-            <p
-              className="text-[#FFBA00] cursor-pointer ml-5 mb-4 text-sm"
-              onClick={() => setLogin(3)}
-            >
-              {t("login-text5")}
-            </p>
+              <div className="mb-4 relative">
+                <label
+                  className="block text-[#828282] text-sm px-5 pb-2"
+                  htmlFor="password"
+                >
+                  {t("login-text4")}
+                </label>
+                <input
+                  type={passwordVisible ? "text" : "password"}
+                  id="password"
+                  placeholder={t("login-text4")}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={`w-full px-4 py-2 border rounded-lg outline-none text-[#000000] ${
+                    errors.password
+                      ? "border-b-2 border-[red]"
+                      : "border-[#ACACAC]"
+                  }`}
+                />
+                <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  className="absolute right-3 top-10 text-gray-500"
+                >
+                  {passwordVisible ? <AiOutlineEye /> : <PiEyeClosedBold />}
+                </button>
+                {errors.password && (
+                  <p className="text-red-500 text-sm mt-1 px-1">
+                    {t("login-text3")}
+                  </p>
+                )}
+              </div>
 
-            <div className="flex gap-6 justify-center items-center">
-              <div className="w-[130px] bg-[#828282] h-[1px]"></div>
-              <p className="text-[#828282]">{t("login-text12")}</p>
-              <div className="w-[130px] bg-[#828282] h-[1px]"></div>
-            </div>
+              <p
+                className="text-[#FFBA00] cursor-pointer ml-5 mb-4 text-sm"
+                onClick={() => setLogin(3)}
+              >
+                {t("login-text5")}
+              </p>
 
-            <div className="flex flex-col justify-between items-center my-5">
-              {/* <Link
+              <div className="flex gap-6 justify-center items-center">
+                <div className="w-[130px] bg-[#828282] h-[1px]"></div>
+                <p className="text-[#828282]">{t("login-text12")}</p>
+                <div className="w-[130px] bg-[#828282] h-[1px]"></div>
+              </div>
+
+              <div className="flex flex-col justify-between items-center my-5">
+                {/* <Link
                 href="/telegram-login.html"
                 className="w-full hidden-on-iphone"
               > */}
@@ -250,39 +254,40 @@ export default function Login({ setLogin, loginCount }) {
                   <RiTelegram2Fill className="bg-[#2AABEE] text-[white] p-1 text-[28px] rounded-full" />
                   {t("login-text6")}
                 </button>
-              {/* </Link> */}
+                {/* </Link> */}
+
+                <button
+                  type="button"
+                  onClick={handleGoogleLogin}
+                  className="flex items-center justify-center text-[black] font-medium text-[20px] leading-[23px] py-2 px-4 rounded-[5px] gap-5 w-full mb-[10px] border-2 border-[#313131] hidden-on-iphone"
+                >
+                  <FcGoogle className="p-0 text-[28px] rounded-full" />
+                  {t("login-text7")}
+                </button>
+                <button
+                  type="button"
+                  onClick={handleAppleLogin}
+                  className="flex items-center justify-center text-[black] font-medium text-[20px] leading-[23px] py-2 px-4 rounded-[5px] gap-5 w-full mb-[10px] border-2 border-[#313131] hidden-on-iphone"
+                >
+                  <IoLogoApple className=" text-[28px] rounded-full" />
+                  {t("login-text8")}
+                </button>
+              </div>
 
               <button
-                type="button"
-                onClick={handleGoogleLogin}
-                className="flex items-center justify-center text-[black] font-medium text-[20px] leading-[23px] py-2 px-4 rounded-[5px] gap-5 w-full mb-[10px] border-2 border-[#313131] hidden-on-iphone"
+                type="submit"
+                className="w-full flex justify-center bg-[#FFBA00] text-[#313131] py-2 px-4 font-medium  rounded-lg mt-2 mb-6 border-2 border-[transparent] border-b-[#313131]"
               >
-                <FcGoogle className="p-0 text-[28px] rounded-full" />
-                {t("login-text7")}
+                {loading ? (
+                  <AiOutlineLoading3Quarters className="animate-spin" />
+                ) : (
+                  t("login")
+                )}
               </button>
-              <button
-                type="button"
-                onClick={handleAppleLogin}
-                className="flex items-center justify-center text-[black] font-medium text-[20px] leading-[23px] py-2 px-4 rounded-[5px] gap-5 w-full mb-[10px] border-2 border-[#313131] hidden-on-iphone"
-              >
-                <IoLogoApple className=" text-[28px] rounded-full" />
-                {t("login-text8")}
-              </button>
-            </div>
-
-            <button
-              type="submit"
-              className="w-full flex justify-center bg-[#FFBA00] text-[#313131] py-2 px-4 font-medium  rounded-lg mt-2 mb-6 border-2 border-[transparent] border-b-[#313131]"
-            >
-              {loading ? (
-                <AiOutlineLoading3Quarters className="animate-spin" />
-              ) : (
-                t("login")
-              )}
-            </button>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
+      </Suspense>
     </>
   );
 }
