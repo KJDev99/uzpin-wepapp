0f6cbc98ad970132e9da4829f8b2050161448722
Omonov <algoritm1202@gmail.com>
2025-07-04 10:02:58 +0500

    games card add mobile legends

--- components/hero/TopGameCard.jsx
+++ components/hero/TopGameCard.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useEffect, useState, useCallback } from "react";
-import Image from "next/image";
 import axiosInstance from "@/libs/axios";
-import Loader from "../Loader";
+import Image from "next/image";
 import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import Loader from "../Loader";
 
 export default function TopGameCards() {
   const { t } = useTranslation();
@@ -43,7 +43,30 @@ export default function TopGameCards() {
         {t("all_games")}
       </h2>
       <div className="overflow-x-auto pb-6">
-        <div className="flex gap-2 min-w-full snap-mandatory">
+        <div className="flex gap-2 min-w-full snap-mandatory px-2">
+          <div className="flex-shrink-0  snap-center w-[80px]">
+            <Link
+              href={`/all-games/1`}
+              className="p-[5px] overflow-hidden flex flex-col items-center"
+            >
+              <div className="relative aspect-square">
+                <Image
+                  src={
+                    "https://api.uzpin.games/media/uploads/2de3e915-e03a-414f-88de-f43f250e0f87.png"
+                  }
+                  alt={"img"}
+                  className="object-cover w-[60px] h-[60px] rounded-lg "
+                  width={60}
+                  height={60}
+                />
+              </div>
+              <div className="pt-2">
+                <h3 className="font-medium truncate text-nowrap text-[#313131] text-xs text-left">
+                  Mobile Legends
+                </h3>
+              </div>
+            </Link>
+          </div>
           {games.map((game, indx) => (
             <div key={indx} className="flex-shrink-0  snap-center w-[80px]">
               <Link
