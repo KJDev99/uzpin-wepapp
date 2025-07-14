9228f2c825e3541ef12f4d67c9331db7fae5ee0d
Omonov <algoritm1202@gmail.com>
2025-07-14 12:15:42 +0500

    push app

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,10 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-      />
       <head>
         <Script src="https://telegram.org/js/telegram-web-app.js?56" />
         <link rel="icon" href="/favicon.ico" />
